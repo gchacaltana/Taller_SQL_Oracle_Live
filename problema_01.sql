93299f662562827ef4a6deb673b1a0b27faccb4b
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-11-21 17:10:41 -0500

    change description problem 01 SQL Oracle Live

--- problema_01.sql
+++ problema_01.sql
@@ -1,5 +1,7 @@
 /**
-Muestre el salario más alto, más bajo, salario total de la planilla y salario promedio de la tabla empleados. Etiquete las columnas como «Salario Máximo», «Salario Mínimo», «Salario Planilla» y «Salario Promedio» respectivamente. Redondee los resultados al valor entero más cercano, con un formato numérico para la moneda de dólares.
+Muestre el salario más alto, más bajo, salario total de la planilla y salario promedio de la tabla empleados. 
+Etiquete las columnas como «Salario Máximo», «Salario Mínimo», «Salario Planilla» y «Salario Promedio» respectivamente. 
+Redondee los resultados al valor entero más cercano, con un formato numérico para la moneda de dólares.
 */
 SELECT
 TO_CHAR(ROUND(MAX(salary)),'$99,999.00') AS "Salario Máximo", 
