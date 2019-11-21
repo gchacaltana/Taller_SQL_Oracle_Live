71f3077d8c82961e66b0e75537e86f8d59b7cdc1
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-11-21 17:19:31 -0500

    add problema 11 sql

---
+++ problema_11.sql
@@ -0,0 +1,10 @@
+/**
+Encuentre a los empleados que tienen más de una rotación laboral.
+*/
+
+SELECT (e.first_name || ' ' || e.last_name) "Empleado",
+COUNT(jh.employee_id) "Rotaciones"
+FROM hr.job_history jh
+INNER JOIN hr.employees e ON jh.employee_id=e.employee_id
+GROUP BY (e.first_name || ' ' || e.last_name)
+HAVING COUNT(jh.employee_id)>1
