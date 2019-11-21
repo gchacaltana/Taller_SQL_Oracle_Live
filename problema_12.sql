6a8eef1540520d7aed78d1ae486b0459fa9d145c
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-11-21 17:20:29 -0500

    add problema 12 sql

---
+++ problema_12.sql
@@ -0,0 +1,10 @@
+/**
+¿Cuál fue el año donde ingresaron más trabajadores?
+*/
+
+SELECT
+TO_CHAR(hire_date, 'YYYY') "Año Contratación",
+COUNT(employee_id) "Empleados"
+FROM hr.employees
+GROUP BY TO_CHAR(hire_date, 'YYYY')
+ORDER BY 2 DESC
