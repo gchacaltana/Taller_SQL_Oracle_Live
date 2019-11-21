6129d2c953ca4e9d9b31bdb140328501f786d14a
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-11-21 17:12:11 -0500

    add problema 02 sql

---
+++ problema_02.sql
@@ -0,0 +1,15 @@
+/**
+Muestre el salario más alto, más bajo, salario total y salario promedio por cada tipo de puesto 
+que se tiene en la organización. Etiquete las columnas como «Puesto», «Salario Máximo», «Salario Mínimo», 
+«Salario Total» y «Salario Promedio» respectivamente. Redondee los resultados con dos cifras decimales, 
+con un formato numérico para la moneda de dólares.
+*/
+
+SELECT
+j.job_title AS "Puesto",
+TO_CHAR(ROUND(MAX(e.salary),2),'$99,999.00') AS "Salario Máximo", 
+TO_CHAR(ROUND(MIN(e.salary),2),'$99,999.00') AS "Salario Mínimo", 
+TO_CHAR(ROUND(SUM(e.salary),2),'$999,999.00') AS "Salario Total",
+TO_CHAR(ROUND(AVG(e.salary),2),'$99,999.00') AS "Salario Promedio"
+FROM hr.employees e INNER JOIN hr.jobs j ON e.job_id=j.job_id
+GROUP BY j.job_title;
