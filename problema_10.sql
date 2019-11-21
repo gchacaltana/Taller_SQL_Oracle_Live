817006827c4ce64eede4e0b92992d2c935c3db2d
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-11-21 17:18:52 -0500

    add problema 10 sql

---
+++ problema_10.sql
@@ -0,0 +1,16 @@
+/**
+Muestre el nombre del Manager, nombre del puesto, nombre de departamento, salario y cantidad 
+de trabajadores que tiene a su cargo. Ordenar según la cantidad de trabajadores en orden descendente.
+*/
+
+SELECT
+(m.first_name || ' ' || m.last_name) "Manager",
+j.job_title "Puesto",
+d.department_name "Departamento",
+TO_CHAR(ROUND(m.salary,2),'$999,999.00') "Salario",
+(SELECT COUNT(employee_id) FROM hr.employees WHERE manager_id = m.employee_id) "Total Empleados"
+FROM hr.employees m
+INNER JOIN hr.jobs j ON m.job_id=j.job_id
+INNER JOIN hr.departments d ON m.department_id=d.department_id
+WHERE m.employee_id IN (SELECT DISTINCT(manager_id) FROM hr.employees)
+ORDER BY 5 DESC
